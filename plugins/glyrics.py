b732b1cb55ef81a01f2045eb457390c3da5b7547
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-02-11 12:50:28 +0530

    Update glyrics.py

--- plugins/glyrics.py
+++ plugins/glyrics.py
@@ -3,6 +3,7 @@
 import lyricsgenius
 
 from userge import Message, userge
+from userge.utils import post_to_telegraph
 
 if GENIUS is not None:
     genius = lyricsgenius.Genius(GENIUS)
@@ -25,10 +26,42 @@ async def lyrics(message: Message):
     if GENIUS is None:
         await message.edit("Provide 'Genius access token' as `GENIUS` to config vars...")
         return
+    
+    to_search = song + "genius lyrics"
+    gen_surl = list(search(to_search, num=1, stop=1))[0]
+    gen_page = requests.get(gen_surl)
+    scp = BeautifulSoup(gen_page.text, "html.parser")
+    writers_box = [
+        writer
+        for writer in scp.find_all("span", {"class": "metadata_unit-label"})
+        if writer.text == "Written By"
+    ]
+    if writers_box:
+        target_node = writers_box[0].find_next_sibling(
+            "span", {"class": "metadata_unit-info"}
+        )
+        writers = target_node.text.strip()
+    else:
+        writers = "Couldn't find..."
+    
     if "-" in song:
         artist, song = song.split("-", 1)
+        artist = artist.strip()
+        song = song.strip()
     await message.edit(f"Searching lyrics for **{artist} - {song}** on Genius...`")
     lyr = genius.search_song(song, artist)
     if lyr is None:
         await message.edit(f"Couldn't find `{artist} - {song}` on Genius...")
         return
+    lyric = lyr.lyrics
+    title = f"{artist} - {song}"
+    lyrics = f"Lyrics for **{title}** by Genius..."
+    lyrics += f"\n\n{lyric}"
+    lyrics += f"\n\n**Written by: **`{writers}`"
+    lyrics += f"\n**Source: **`genius.com`"
+    if len(lyrics) <= 4096:
+        await message.edit(f"{lyrics}")
+    else:
+        lyrics = lyrics.replace("\n", "<br>")
+        link = post_to_telegraph(f"Lyrics for {title}...", lyrics)
+        await message.edit(f"Lyrics for **{title}** by Genius.com...\n[Link]({link})")
