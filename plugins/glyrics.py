0c2d76ee420ce6272e9a07b866a1c5f44e856713
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-05-04 20:37:31 +0530

    Update glyrics.py

--- plugins/glyrics.py
+++ plugins/glyrics.py
@@ -123,7 +123,7 @@ async def lyrics(message: Message):
         else:
             lyrics = lyrics.replace("\n", "<br>")
             link = post_to_telegraph(f"Lyrics for {title}...", lyrics)
-            await message.edit(f"Lyrics for <a href='{link}'>{title}</a> by genius.com...\n<a href='{link}'>Link</a>", disable_web_page_preview=True)
+            await message.edit(f"Lyrics for <a href='{link}'><b>{title}</b></a> by genius.com...\n<a href='{link}'>Link</a>", disable_web_page_preview=True)
         return
     if lyr is None:
         await message.edit(f"Couldn't find `{title}` on Genius...")
@@ -140,4 +140,4 @@ async def lyrics(message: Message):
     else:
         lyrics = lyrics.replace("\n", "<br>") 
         link = post_to_telegraph(f"Lyrics for {title}...", lyrics)
-        await message.edit(f"Lyrics for <a href='{link}'>{title}</a> by genius.com...\n<a href='{link}'>Link</a>", disable_web_page_preview=True)
+        await message.edit(f"Lyrics for <a href='{link}'><b>{title}</b></a> by genius.com...\n<a href='{link}'>Link</a>", disable_web_page_preview=True)
